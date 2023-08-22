d50dba23b6ed43b52baa481597e812dee0cac4c6
Andrey Tikhonov <andrey.tikhonov@quantori.com>
2023-08-23 01:17:04 +0400

    Refactor aiogram middleware

--- src/sulguk/aiogram_middleware.py
+++ src/sulguk/aiogram_middleware.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any
+from typing import Any, Callable, Dict, Type, TypeVar
 
 from aiogram import Bot
 from aiogram.client.session.middlewares.base import (
@@ -12,7 +12,7 @@ from aiogram.methods import (
 )
 from aiogram.methods.base import TelegramType
 from aiogram.types import (
-    InlineQueryResultArticle, UNSET_PARSE_MODE
+    InlineQueryResult, InlineQueryResultArticle, UNSET_PARSE_MODE
 )
 
 from sulguk.data import SULGUK_PARSE_MODE
@@ -20,71 +20,87 @@ from .wrapper import transform_html
 
 logger = logging.getLogger(__name__)
 
+M = TypeVar("M", bound=TelegramMethod)
+Handler = Callable[[M, Bot], None]
+
 
 class AiogramSulgukMiddleware(BaseRequestMiddleware):
-    async def __call__(
-            self,
-            make_request: NextRequestMiddlewareType[TelegramType],
-            bot: "Bot",
-            method: TelegramMethod[TelegramType],
-    ) -> Response[TelegramType]:
-        handlers = {
+    def __init__(self):
+        self.handlers: Dict[Type[TelegramMethod], Handler] = {
             EditMessageMedia: self._process_edit_message_media,
             SendMediaGroup: self._process_send_media_group,
             AnswerWebAppQuery: self._process_answer_web_app_query,
             AnswerInlineQuery: self._process_answer_inline_query
         }
 
-        handler = handlers.get(type(method), self._process_generic)
+    async def __call__(
+            self,
+            make_request: NextRequestMiddlewareType[TelegramType],
+            bot: Bot,
+            method: TelegramMethod[TelegramType],
+    ) -> Response[TelegramType]:
+        handler = self.handlers.get(type(method), self._process_generic)
         handler(method, bot)
-
         return await make_request(bot, method)
 
-    def _process_inline_query_result(self, result, bot: "Bot"):
-        target = result.input_message_content if isinstance(result, InlineQueryResultArticle) else result
+    def _process_inline_query_result(
+            self, method: InlineQueryResult, bot: Bot,
+    ) -> None:
+        if isinstance(method, InlineQueryResultArticle):
+            target = method.input_message_content
+        else:
+            target = method
         self._transform_text_caption(target, bot)
 
-    def _process_answer_inline_query(self, object: AnswerInlineQuery, bot: "Bot"):
-        for result in object.results:
+    def _process_answer_inline_query(
+            self, method: AnswerInlineQuery, bot: Bot,
+    ) -> None:
+        for result in method.results:
             self._process_inline_query_result(result, bot)
 
-    def _process_answer_web_app_query(self, object: AnswerWebAppQuery, bot: "Bot"):
-        self._process_inline_query_result(object.result, bot)
+    def _process_answer_web_app_query(
+            self, method: AnswerWebAppQuery, bot: Bot,
+    ) -> None:
+        self._process_inline_query_result(method.result, bot)
 
-    def _process_edit_message_media(self, object: EditMessageMedia, bot: "Bot"):
-        self._transform_text_caption(object.media, bot)
+    def _process_edit_message_media(
+            self, method: EditMessageMedia, bot: Bot,
+    ) -> None:
+        self._transform_text_caption(method.media, bot)
 
-    def _process_send_media_group(self, object: SendMediaGroup, bot: "Bot"):
-        for media in object.media:
+    def _process_send_media_group(
+            self, method: SendMediaGroup, bot: Bot,
+    ) -> None:
+        for media in method.media:
             self._transform_text_caption(media, bot)
 
-    def _process_generic(self, object: Any, bot: "Bot"):
-        self._transform_text_caption(object, bot)
+    def _process_generic(
+            self, method: Any, bot: Bot,
+    ) -> None:
+        self._transform_text_caption(method, bot)
 
-    def _transform_text_caption(self, object: Any, bot: "Bot"):
-
-        if self._check_pars_mode(object, bot):
+    def _transform_text_caption(
+            self, method: Any, bot: Bot,
+    ) -> None:
+        if not self._is_parse_mode_supported(method, bot):
             return
 
-        content_attribute = None
-        if hasattr(object, "caption"):
-            content_attribute = 'caption'
-        elif hasattr(object, "text"):
-            content_attribute = 'text'
+        if hasattr(method, "caption"):
+            result = transform_html(method.caption)
+            method.caption = result.text
+            method.caption_entities = result.entities
+        elif hasattr(method, "text"):
+            result = transform_html(method.text)
+            method.text = result.text
+            method.entities = result.entities
+        else:
+            raise ValueError(
+                "The method does not have a 'caption' or 'text' attribute.",
+            )
+        method.parse_mode = None
 
-        if not content_attribute:
-            raise ValueError("The object does not have a 'caption' or 'text' attribute.")
-
-        original_content = getattr(object, content_attribute)
-        transformed_content = transform_html(original_content)
-
-        setattr(object, content_attribute, transformed_content.text)
-        setattr(object, "entities", transformed_content.entities)
-
-        object.parse_mode = None
-
-    def _check_pars_mode(self, object: Any, bot: "Bot"):
-        parse_mode = getattr(object, "parse_mode", "")
-        is_obj_sulguk = parse_mode == SULGUK_PARSE_MODE
-        is_bot_unset = bot.parse_mode == SULGUK_PARSE_MODE and parse_mode == UNSET_PARSE_MODE
-        return not (is_obj_sulguk or is_bot_unset)
+    def _is_parse_mode_supported(self, method: Any, bot: Bot) -> bool:
+        parse_mode = getattr(method, "parse_mode", "")
+        if parse_mode is UNSET_PARSE_MODE:
+            parse_mode = bot.parse_mode
+        return parse_mode == SULGUK_PARSE_MODE
